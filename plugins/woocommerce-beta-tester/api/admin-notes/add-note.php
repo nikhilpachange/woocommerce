9f11d0f8d74ee91f63eaaa524bbcb4fe892886bf
Nikhil  Pachange <nikhil1pachange@gmail.com>
2025-09-23 20:19:28 +0530

    Update add-note.php

--- plugins/woocommerce-beta-tester/api/admin-notes/add-note.php
+++ plugins/woocommerce-beta-tester/api/admin-notes/add-note.php
@@ -4,80 +4,87 @@ defined( 'ABSPATH' ) || exit;
 
 use Automattic\WooCommerce\Admin\Notes\Note;
 
+// Register a test REST route for adding custom notes
 register_woocommerce_admin_test_helper_rest_route(
-	'/admin-notes/add-note/v1',
-	'admin_notes_add_note'
+	'/admin-notes/create-note/v1',
+	'wc_admin_create_note'
 );
 
 /**
- * Adds an admin note.
+ * REST callback to create a WooCommerce admin note.
  *
- * @param WP_REST_Request $request Full data about the request.
+ * @param WP_REST_Request $request REST request object containing parameters.
+ * @return bool
  */
-function admin_notes_add_note( $request ) {
-	$note           = new Note();
-	$mock_note_data = get_mock_note_data();
-	$type           = $request->get_param( 'type' );
-	$layout         = $request->get_param( 'layout' );
+function wc_admin_create_note( $request ) {
+	$note      = new Note();
+	$mock_data = wc_admin_mock_note_data();
+
+	$type   = $request->get_param( 'type' );
+	$layout = $request->get_param( 'layout' );
 
 	$note->set_name( $request->get_param( 'name' ) );
 	$note->set_title( $request->get_param( 'title' ) );
-	$note->set_content( $mock_note_data['content'] );
-	$note->set_image( $mock_note_data[ $type ][ $layout ] );
+	$note->set_content( $mock_data['content'] ?? '' );
+	$note->set_image( $mock_data[ $type ][ $layout ] ?? '' );
 	$note->set_layout( $layout );
 	$note->set_type( $type );
-	possibly_add_action( $note );
+
+	wc_admin_maybe_add_action( $note );
 
 	if ( 'email' === $type ) {
-		add_email_note_params( $note );
+		wc_admin_add_email_params( $note );
 	}
 
 	$note->save();
-
 	return true;
 }
 
 /**
- * Adds an email note parameter.
+ * Append extra parameters for email-type notes.
  *
- * @param Note $note The note to add parameters to.
+ * @param Note $note The note instance.
+ * @return void
  */
-function add_email_note_params( $note ) {
-	$additional_data = array(
+function wc_admin_add_email_params( $note ) {
+	$email_data = array(
 		'role' => 'administrator',
 	);
-	$note->set_content_data( (object) $additional_data );
+
+	$note->set_content_data( (object) $email_data );
 }
 
 /**
- * Possibly adds an action to a note.
+ * Conditionally attach an action button to a note.
  *
- * @param Note $note The note to check and add an action to.
+ * @param Note $note The note to update.
+ * @return void
  */
-function possibly_add_action( $note ) {
-	if ( $note->get_type() === 'info' ) {
+function wc_admin_maybe_add_action( $note ) {
+	if ( 'info' === $note->get_type() ) {
 		return;
 	}
-	$action_name = sprintf(
-		'test-action-%s',
-		$note->get_name()
-	);
-	$note->add_action( $action_name, 'Test action', wc_admin_url() );
+
+	$action_id = sprintf( 'wc-test-action-%s', $note->get_name() );
+	$note->add_action( $action_id, __( 'Test Action', 'woocommerce' ), wc_admin_url() );
 }
 
 /**
- * Gets mock note data.
+ * Provide fake/mock note data for testing purposes.
+ *
+ * @return array
  */
-function get_mock_note_data() {
-	$plugin_url = site_url() . '/wp-content/plugins/woocommerce-admin-test-helper/';
+function wc_admin_mock_note_data() {
+	$plugin_assets = site_url( '/wp-content/plugins/woocommerce-admin-test-helper/' );
+
 	return array(
-		'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+		'content' => __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in lacus at nulla auctor aliquet.', 'woocommerce' ),
 		'info'    => array(
-			'thumbnail' => $plugin_url . 'images/admin-notes/thumbnail.jpg',
+			'thumbnail' => $plugin_assets . 'images/admin-notes/thumbnail.jpg',
 			'plain'     => '',
 		),
 		'email'   => array(
-			'plain' => $plugin_url . 'images/admin-notes/woocommerce-logo-vector.png',
+			'plain' => $plugin_assets . 'images/admin-notes/woocommerce-logo-vector.png',
 		),
 		'update'  => array(
 			'plain' => '',
