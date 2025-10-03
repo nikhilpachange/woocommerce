5fddfa321c62f9fa003a5ff4a1cccccd1d303205
Nikhil  Pachange <nikhil1pachange@gmail.com>
2025-10-04 01:27:22 +0530

    Update magic-button.test.js

--- plugins/woo-ai/src/components/magic-button/magic-button.test.js
+++ plugins/woo-ai/src/components/magic-button/magic-button.test.js
@@ -1,31 +1,19 @@
-/**
- * External dependencies
- */
 import { render, fireEvent } from '@testing-library/react';
-
-/**
- * Internal dependencies
- */
 import { MagicButton } from './magic-button';
 
-describe( 'MagicButton', () => {
-	it( 'renders correctly', () => {
-		const { getByText } = render(
-			<MagicButton onClick={ () => {} } label="Test Button" />
-		);
+describe('MagicButton', () => {
+  const renderButton = (props = {}) => render(<MagicButton label="Test Button" {...props} />);
 
-		expect( getByText( 'Test Button' ) ).toBeInTheDocument();
-	} );
+  it('renders correctly', () => {
+    const { getByText } = renderButton();
+    expect(getByText('Test Button')).toBeInTheDocument();
+  });
 
-	it( 'calls onClick prop when clicked', () => {
-		const handleClick = jest.fn();
+  it('calls onClick prop when clicked', () => {
+    const handleClick = jest.fn();
+    const { getByText } = renderButton({ onClick: handleClick });
 
-		const { getByText } = render(
-			<MagicButton onClick={ handleClick } label="Test Button" />
-		);
-
-		fireEvent.click( getByText( 'Test Button' ) );
-
-		expect( handleClick ).toHaveBeenCalledTimes( 1 );
-	} );
-} );
+    fireEvent.click(getByText('Test Button'));
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
